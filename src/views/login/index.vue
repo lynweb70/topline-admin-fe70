9cfc819fa93dade65a4fbee02fc556db4e52f154
刘雅楠 <lynweb70@163.com>
2019-06-24 08:07:58 +0800

    第三次提交

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,23 +3,39 @@
     <!-- 给组件加 class，会把这个 class 作用到组件的根元素上 -->
     <div class="form-wrap">
       <div class="form-head">
-        <img src="../../assets/avatar.jpg" alt="黑马头条号">
+        <img src=""
+             alt="黑马头条号">
       </div>
-      <el-form class="form-content" ref="form" :model="form">
-        <el-form-item>
-          <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+      <el-form class="form-content"
+               ref="form"
+               :model="form"
+               :rules="rules">
+        <el-form-item prop="mobile">
+          <el-input v-model="form.mobile"
+                    placeholder="手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-col :span="14">
-            <el-input v-model="form.code" placeholder="验证码"></el-input>
+            <el-input v-model="form.code"
+                      placeholder="验证码"></el-input>
           </el-col>
-          <el-col :offset="1" :span="9">
-            <el-button @click="handleSendCode">获取验证码</el-button>
+          <el-col :offset="1"
+                  :span="9">
+            <!-- <el-button @click="handleSendCode">获取验证码</el-button> -->
+            <el-button @click="handleSendCode"
+                       :disabled="!!codeTimer">{{ codeTimer ? `剩余${codeTimeSeconds}秒` : '获取验证码' }}</el-button>
           </el-col>
+        </el-form-item>
+        <el-form-item prop="agree">
+          <el-checkbox class="agree-checkbox"
+                       v-model="form.agree"></el-checkbox>
+          <span class="agree-text">我已阅读并同意<a href=" ">用户协议</a>和<a href="#">隐私条款</a></span>
         </el-form-item>
         <el-form-item>
           <!-- <el-button class="btn-login" type="primary" @click="onSubmit">登录</el-button> -->
-          <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
+          <el-button class="btn-login"
+                     type="primary"
+                     @click="handleLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -29,14 +45,35 @@
 <script>
 import axios from 'axios'
 import '@/vendor/gt'
+const initCodeTimeSeconds = 10
 export default {
   name: 'AppLogin',
   data () {
     return {
       form: {
         mobile: '',
-        code: ''
-      }
+        code: '',
+        agree: ''
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          // { len: 11, message: '长度必须为11位', trigger: 'blur' }
+          { pattern: /\d{11}/, message: '请输入有效的手机号码', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          // { len: 6, message: '长度必须为6位', trigger: 'blur' }
+          { pattern: /\d{6}/, message: '请输入有效的验证码', trigger: 'blur' }
+        ],
+        agree: [
+          { required: true, message: '请同意用户协议' },
+          { pattern: /true/, message: '请同意用户协议' }
+        ]
+
+      },
+      codeTimer: null, // 倒计时定时器
+      codeTimeSeconds: initCodeTimeSeconds // 倒计时事件
     }
   },
   methods: {
@@ -44,12 +81,24 @@ export default {
     //   console.log('submit!')
     // },
     handleLogin () {
+      // 使用 form 组件的 validate 方法触发校验，获取校验的结果状态
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        // 表单验证通过，提交登录请求
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
         data: this.form
       }).then(res => {
         console.log(res.data)
+        const userInfo = res.data.data
+        window.localStorage.setItem('user_info', JSON.stringify(userInfo))
         this.$message({
           message: '登录成功',
           type: 'success'
@@ -62,6 +111,16 @@ export default {
       })
     },
     handleSendCode () {
+      // 验证手机号是否有效
+      this.$refs['form'].validateField('mobile', errorMessage => {
+        if (errorMessage.trim().length > 0) {
+          return
+        }
+        // 验证通过，初始化显示验证码
+        this.showGeetest()
+      })
+    },
+    showGeetest () {
       const { mobile } = this.form
       axios({
         method: 'GET',
@@ -75,17 +134,17 @@ export default {
           offline: !data.success,
           new_captcha: data.new_captcha,
           product: 'bind'
-        }, function (captchaObj) {
-          captchaObj.onReady(function () {
+        }, captchaObj => {
+          captchaObj.onReady(() => {
             captchaObj.verify()
-          }).onSuccess(function () {
+          }).onSuccess(() => {
             // your code
             console.log(captchaObj.getValidate())
             const {
               geetest_challenge: challenge,
               geetest_seccode: seccode,
               geetest_validate: validate } =
-                captchaObj.getValidate()
+              captchaObj.getValidate()
             axios({
               method: 'GET',
               url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
@@ -95,45 +154,62 @@ export default {
                 seccode
               }
             }).then(res => {
-              console.log(res.data)
+              // console.log(res.data)
+              this.codeCountDown()
             })
           }).onError(function () {
             // your code
           })
         })
       })
+    },
+    codeCountDown () {
+      this.codeTimer = window.setInterval(() => {
+        this.codeTimeSeconds--
+        if (this.codeTimeSeconds <= 0) {
+          // 清除定时器
+          window.clearInterval(this.codeTimer)
+          // 让倒计时的时间回归初始状态
+          this.codeTimeSeconds = initCodeTimeSeconds
+          // 将存储定时器引用的变量重新赋值为 null
+          this.codeTimer = null
+        }
+      }, 1000)
     }
   }
 }
 </script>
 <style lang="less" scoped>
-  .login-wrap {
-    height: 100%;
-    background-color: #2b3e4a;
+.login-wrap {
+  height: 100%;
+  background-color: #2b3e4a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-head {
     display: flex;
     justify-content: center;
     align-items: center;
-
-    .form-head {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-bottom: 10px;
-
-      img {
-        width: 50px;
-      }
-    }
-
-    .form-wrap {
-      width: 400px;
-      background-color: #fff;
-      padding: 20px;
-      border-radius: 10px;
-
-      .btn-login {
-        width: 100%;
-      }
+    margin-bottom: 10px;
+    img {
+      width: 200px;
     }
   }
+  .form-wrap {
+    width: 400px;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 10px;
+    .agree-checkbox {
+      margin-right: 10px;
+    }
+    .agree-text {
+      font-size: 16px;
+      color: #999;
+    }
+    .btn-login {
+      width: 100%;
+    }
+  }
+}
 </style>
